5957df8883d120c4801707a6c99bf413b8d02fa7
UsikB <ni.github@gmail.com>
2022-10-17 14:14:23 +0300

    for review

--- src/main/java/uipattern/page/WaitForLoad.java
+++ src/main/java/uipattern/page/WaitForLoad.java
@@ -1,4 +1,22 @@
 package uipattern.page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class WaitForLoad {
+
+    static WebDriver driver;
+
+    WaitForLoad(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    static void waitForLoadXpath(By xpath) {
+        new WebDriverWait(driver, 3)
+                .until(ExpectedConditions.visibilityOfElementLocated(xpath));
+    }
+
+
 }
